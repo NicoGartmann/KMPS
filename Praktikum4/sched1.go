2ada802faa75722bb1ae3c6fe9d0900ba418e928
NicoGartmann <nico.gartmann97gmail.com>
2020-12-09 20:48:42 +0100

    p4

--- Praktikum4/sched1.go
+++ Praktikum4/sched1.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime"
 	"runtime/trace"
+	"time"
 )
 
 func main() {
-	trace.Start(os.Stderr)
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
 	defer trace.Stop()
+	runtime.GOMAXPROCS(1) // ... limitiere die nutzbaren Prozessoren auf einen.
+	fmt.Printf("runtime.GOMAXPROCS(0) returned %d CPUs\n", runtime.GOMAXPROCS(0))
 
-	ch := make(chan int)
+	go func(my_name string) {
+		for i := 0; i < 10; i++ {
+			fmt.Println(my_name, ": ", i)
+		}
+	}("goroutine1")
+	go func(my_name string) {
+		for i := 0; i < 10; i++ {
+			fmt.Println(my_name, ": ", i)
+		}
+	}("goroutine2")
+	go func(my_name string) {
+		for i := 0; i < 10; i++ {
+			fmt.Println(my_name, ": ", i)
+		}
+	}("goroutine3")
 
-	go func() {
-		ch <- 42
-	}()
-
-	<-ch
+	time.Sleep(10 * time.Second)
 }
