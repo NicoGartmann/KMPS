8ac1900a10f7b66d69d0776a8f64e0afdd417aef
NicoGartmann <nico.gartmann97@gmail.com>
2021-01-11 08:09:25 +0100

    anpassung abgabe

--- Praktikum5/client.js
+++ Praktikum5/client.js
@@ -1,85 +1,23 @@
-const WebSocket = require('ws');
+const ws = require('ws'); 
 const prompt = require('prompt-sync')();
-var readline = require('readline');
+const input = require('readline'); 
 
-var rl = readline.createInterface({
+var rl = input.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+ 
+console.log('Client gestartet.'); 
+const new_client = prompt('Bitte Client-ID eingeben: ');
 
-var tickets;
-console.log("Client gestartet");
-const name = prompt('Bitte Client-ID eingeben: ');
-console.log();
-console.log();
-let ws;
-if (ws) {
-  ws.onerror = ws.onopen = ws.onclose = null;
-  ws.close();
+connection = new ws('ws://localhost:3000'); 
+
+connection.onopen = function() {
+  console.log('Verbindung hergestellt.\n'); 
+  connection.send(JSON.stringify("Hello Server"));
 }
 
-ws = new WebSocket('ws://localhost:3000');
-ws.onopen = () => {
-  var nameArr = Array();
-  nameArr.push(name);
-  ws.send(JSON.stringify(nameArr));
-}
-ws.onclose = function() {
-  ws = null;
-}
-ws.on('message', function incoming(data) {
-    tickets = JSON.parse(data);
-    
-    showTickets(tickets);
-})
-
-function showTickets(data) {
-  
-  if( data.length == 0 ) {
-    console.log(`Client: ${name}`);
-    console.log("Keine Tickets");
-  } else {
-    console.log(`Tickets:`);
-    data.forEach(dat => {
-      console.log(dat);
-    });
-  }
-  rl.question("number Selbstzuw. q: quit ", function(answer) {
-    if(answer == `q`) {
-      process.exit();
-    }else{
-      sendAlloc(answer);
-    }
-  });
-
-  console.log();
-  console.log();
-}
-
-function sendAlloc(i) {
-
-  if(i <= tickets.length && tickets[i-1].indexOf("client") == -1) {
-    var arr = Array();
-    arr.push(i);
-    arr.push(name);
-    ws.send(JSON.stringify(arr));
-  } else {
-
-    if(i > tickets.length) {
-      console.log("Ticket existiert nicht!");
-    }else{
-      console.log("Ticket ist bereits vergeben!");
-    }
-
-    rl.question("number Selbstzuw. q: quit ", function(answer) {
-      if(answer == `q`) {
-        process.exit();
-      }else{
-        sendAlloc(answer);
-      }
-    });
-
-    console.log();
-    console.log();
-  }
+connection.onclose = function () {
+  console.log('Verbindung verloren.\n'); 
+  connection.close();
 }
--- Praktikum5/server.js
+++ Praktikum5/server.js
@@ -1,113 +1,30 @@
-const WebSocket = require('ws');
-var readline = require('readline');
+const ws = require('ws');
+const input = require('readline');
 
-const port = 3000;
-const wss = new WebSocket.Server({ port });
-var clients = new Array();
-var tickets = new Array();
-var alloc = new Array();
-var rl = readline.createInterface({
+const port = 3000; 
+const server = new ws.Server({ port });
+
+const clients = new Array(); 
+
+var rl = input.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-console.log("Server gestartet");
-console.log("Keine Tickets");
-rl.question("n: neues Ticket. q: quit ", function(answer) {
-  if(answer == `n`) {
-    newTicket();
+console.log("Server gestartet.\nKeine Tickets.");
+rl.question("n: neues Ticket, q: Quit\n", function(answer) {
+  if( answer == 'n' ) {
+      
+  } else if( answer == 'q') {
+    process.exit(); 
   }
-  if(answer == `q`) {
-    process.exit();
-  }
-});
-console.log();
-console.log();
+}); 
+
+server.on('connection', function (new_client) {
+  console.log('neuer Client: ', new_client)
+  clients.push(JSON.parse(new_client.data)); 
+  console.log(clients); 
+}); 
 
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    var message = JSON.parse(data);
-    if(message[0].startsWith("client")) {
-      printNewClient(JSON.parse(data), ws);
-    }else{
-      setTicket(parseInt(JSON.parse(data)[0])-1, JSON.parse(data)[1]);
-    }
-  })
-})
 
-
-function printNewClient(data, socket) {
-  clients.push(data);
-  console.log(`Neuer Client: ${data}`);
-  
-  if(tickets.length == 0) {
-    console.log(`Keine Tickets`);
-  }else{
-    tickets.forEach(ticket => {
-      console.log(ticket);
-    });
-  }
-
-  socket.send(JSON.stringify(tickets));
-  
-  rl.question("n: neues Ticket. q: quit ", function(answer) {
-    if(answer == `n`) {
-      newTicket();
-    }
-    if(answer == `q`) {
-      process.exit();
-    }
-  });
-  console.log();
-  console.log();
-  
-}
-
-function newTicket() {
-  tickets.push("ticket" + (tickets.length+1).toString() + " (nicht zugewiesen)");
-  alloc.push("nope");
-  console.log("Tickets:");
-  tickets.forEach(ticket => {
-    console.log(ticket);
-  });
-  broadcastTickets();
-  rl.question("n: neues Ticket. q: quit ", function(answer) {
-    if(answer == `n`) {
-      newTicket();
-    }
-    if(answer == `q`) {
-      process.exit();
-    }
-  });
-  console.log();
-  console.log();
-}
-
-function setTicket(index, cli) {
-  if(alloc[index] == "nope") {
-    alloc[index] = cli;
-    tickets[index] = "ticket" + (index+1).toString() + " (" + cli + ")";
-  }
-  console.log("Tickets:");
-  tickets.forEach(ticket => {
-    console.log(ticket);
-  });
-  rl.question("n: neues Ticket. q: quit ", function(answer) {
-    if(answer == `n`) {
-      newTicket();
-    }
-    if(answer == `q`) {
-      process.exit();
-    }
-  });
-  console.log();
-  console.log();
-  broadcastTickets();
-}
-
-function broadcastTickets() {
-  wss.clients.forEach(function each(client) {
-    client.send(JSON.stringify(tickets));
- });
-}
